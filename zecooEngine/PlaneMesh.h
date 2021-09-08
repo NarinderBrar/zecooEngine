5126c4a832a6ab006fe1e3b168c148ef1d8540bb
Narinder <narinder.singh@chitakara.edu.in>
2021-09-08 13:36:54 +0530

    stretch uv

--- zecooEngine/PlaneMesh.h
+++ zecooEngine/PlaneMesh.h
@@ -6,18 +6,23 @@
 class PlaneMesh
 {
 private:
-    float vertices[32] = {
+    float vertices[48] = {
         // position            normal           uv
-         0.5f,  0.0f, 0.5f,    0.0, -1.0, 0.0,  1.0, 1.0, // top right
-         0.5f,  0.0f, -0.5f,   0.0, -1.0, 0.0,  1.0, 0.0, // bottom right
-        -0.5f,  0.0f, -0.5f,   0.0, -1.0, 0.0,  0.0, 0.0, // bottom left
-        -0.5f,  0.0f, 0.5f,    0.0, -1.0, 0.0,  0.0, 1.0 // top left 
+         -0.5f,  0.0f, -0.5f,    0.0, 1.0, 0.0,  1.0, 1.0, // top right //0
+          0.5f,  0.0f, -0.5f,    0.0, 1.0, 0.0,  0.0, 1.0, // top left  //1 
+        - 0.5f,  0.0f,  0.5f,   0.0, 1.0, 0.0,  1.0, 0.5, // bottom right //2
+          0.5f,  0.0f,  0.5f,   0.0, 1.0, 0.0,  0.0, 0.5, // bottom left // 3
+
+       - 0.5f,  0.0f, 1.5f,    0.0, 1.0, 0.0,  1.0, 0.0, // bottom right // 4
+         0.5f,  0.0f, 1.5f,    0.0, 1.0, 0.0,  0.0, 0.0 // bottom left // 5
     };
 
-    unsigned int indices[6] = 
+    unsigned int indices[12] = 
     {
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
+        0, 1, 2, // first triangle
+        1, 3, 2,  // second triangl
+        2,3,4,
+        3,5,4
     };
 
     unsigned int VBO, VAO, EBO;
